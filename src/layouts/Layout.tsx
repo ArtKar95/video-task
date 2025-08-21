6269f7156f926a342b7fd19a5a6a3f7b2da823ef
ArtKar95 <70438665+ArtKar95@users.noreply.github.com>
2025-08-22 03:49:20 +0400

    feat: setup rtk add home page UI and functionality

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -6,15 +6,15 @@ export default function MainLayout() {
   const [isSidebarHovered, setIsSidebarHovered] = useState(false);
 
   return (
-    <div className="flex h-screen">
+    <div className="flex h-screen w-full">
       <Sidebar
         isSidebarHovered={isSidebarHovered}
         setIsSidebarHovered={setIsSidebarHovered}
       />
 
       <div
-        className={`flex-1 p-4 overflow-y-auto transition-all duration-300  ${
-          isSidebarHovered ? "bg-black/80" : "bg-transparent"
+        className={`flex-1 overflow-y-auto transition-all duration-300 bg-black/100 text-white ${
+          isSidebarHovered ? "opacity-60" : "opacity-100"
         }`}
       >
         <Outlet />
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { Provider } from "react-redux";
 import App from "./app/App.tsx";
+import { store } from "./app/store/index.ts";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
+);
